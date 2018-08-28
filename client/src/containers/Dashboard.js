15756f97c56d5c8763e0f9d01cfc37e341dcc926
George Colon <geocolon@gmail.com>
2018-08-28 15:35:23 -0700

    Css update

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -51,7 +51,7 @@ export class Dashboard extends React.Component {
               />{' '}
               <br />
               <center>
-                <button type="submit">Submit</button>
+                <button className="submit_btn" type="submit">Submit</button>
               </center>
             </form>
           </div>
